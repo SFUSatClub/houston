149136a8f48d8cb18e9dee8f48001fff2f820906
Richard Arthurs <rarthurs@sfu.ca>
2018-04-02 00:01:57 -0700

    working button, text functon on enter, demo list

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import ObjectProperty
 from kivy.lang import Builder
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.label import Label
+from kivy.properties import BooleanProperty
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
 
 class MainTab(BoxLayout):
@@ -20,28 +28,67 @@ class MainTab(BoxLayout):
 
 class Test(TabbedPanel):
 
-    def on_enter(instance, value):
-        print('User pressed enter in', instance)
-    textinput = TextInput(text='Hello world', multiline=False)
 
-    textinput.bind(on_text_validate=on_enter)
-    # txt_inpt = ObjectProperty(None)
-    #
-    # def check_status(self, btn):
-    #     print('text input text is: {txt}'.format(txt=self.txt_inpt))
+    # textinput = TextInput(text='Hello world', multiline=False)
 
-
-
+    # textinput.bind(on_text_validate=on_enter)
 
     pass
+
+
+
+items = [0, "apple", "dog", 1, "banana", "cat", 2, "pear", "rat", 3,  "pineapple", "bat"]
+
+class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior,
+                                RecycleGridLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
+
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in items]
+
+
 
 class TabbedPanelApp(App):
     def build(self):
         return Test()
 
-    def son(self, *args):
+    def button_press(self, *args):
         print("hello")
 
+    def on_enter(self, *args): # gets text from the input box on enter
+        thing = args[0]
+        print(thing.text)
 
 
 
