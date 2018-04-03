2989e3f039e14e49dd1378f6bd11db475084abe6
richarthurs <arthursrich@gmail.com>
2018-04-02 23:53:06 -0700

    Working - cleanup layout, append to list

--- houston.py
+++ houston.py
@@ -72,6 +72,9 @@ class MainTab(BoxLayout):
     def insert(self, value):
         self.rv.data.insert(0, {'value': value or 'default value'})
 
+    def insert_end(self, value):
+        self.rv.data.append({'value': value or 'default value'})
+
     def update(self, value):
         if self.rv.data:
             self.rv.data[0]['value'] = value or 'default new value'
@@ -94,72 +97,6 @@ class TPI2(TabbedPanelItem):
 
 items = [0, "apple", "dog", 1, "banana", "cat", 2, "pear", "rat", 3,  "pineapple", "bat"]
 
-# class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior,
-#                                 RecycleGridLayout):
-#     ''' Adds selection and focus behaviour to the view. '''
-#
-#
-# class SelectableLabel(RecycleDataViewBehavior, Label):
-#     ''' Add selection support to the Label '''
-#     index = None
-#     selected = BooleanProperty(False)
-#     selectable = BooleanProperty(True)
-#
-#     def refresh_view_attrs(self, rv, index, data):
-#         ''' Catch and handle the view changes '''
-#         self.index = index
-#         return super(SelectableLabel, self).refresh_view_attrs(
-#             rv, index, data)
-#
-#     def on_touch_down(self, touch):
-#         ''' Add selection on touch down '''
-#         if super(SelectableLabel, self).on_touch_down(touch):
-#             return True
-#         if self.collide_point(*touch.pos) and self.selectable:
-#             return self.parent.select_with_touch(self.index, touch)
-#
-#     def apply_selection(self, rv, index, is_selected):
-#         ''' Respond to the selection of items in the view. '''
-#         self.selected = is_selected
-#         if is_selected:
-#             print("selection changed to {0}".format(rv.data[index]))
-#         else:
-#             print("selection removed for {0}".format(rv.data[index]))
-
-
-# class RV(RecycleView):
-#     def __init__(self, **kwargs):
-#         super(RV, self).__init__(**kwargs)
-#         self.data = [{'text': str(x)} for x in items]
-#
-#     def rv_foo(self, input):
-#         print(input)
-#         # thingy =[4, "airplane", "hello",]
-#         # self.data.append({'text': str(x)} for x in thingy)
-#         # print(self.data)
-#
-#
-#     def populate(self):
-#         self.data = [{'value': ''.join(sample(ascii_lowercase, 6))}
-#                         for x in range(50)]
-#
-#     def sort(self):
-#         self.data = sorted(self.data, key=lambda x: x['value'])
-#
-#     def clear(self):
-#         self.data = []
-#
-#     def insert(self, value):
-#         self.data.insert(0, {'value': value or 'default value'})
-#
-#     def update(self, value):
-#         if self.data:
-#             self.data[0]['value'] = value or 'default new value'
-#             self.refresh_from_data()
-#
-#     def remove(self):
-#         if self.data:
-#             self.data.pop(0)
 
 class Top(TabbedPanel): # top of the visual hierarchy, builds the tabbed panels
     def __init__(self):
@@ -199,7 +136,7 @@ class Top(TabbedPanel): # top of the visual hierarchy, builds the tabbed panels
         self.tpi2.ids.lb1.text = new_text # a top level ish label
         self.tpi1.mt1.do_stuff(str(random.random() * 100)) # this one demonstrates using the object property passup thing
         # self.tpi1.mt1.rv_do_sthng() #CDS 6 - going down in the hierarchy a little, call the method that pokes down into the child object's methods
-        self.tpi1.mt1.insert('sdfsd')
+        self.tpi1.mt1.insert_end('sdfsd')
 
         # print(self.tpi2.children)
     pass
